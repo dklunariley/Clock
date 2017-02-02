20e59fa91cb49c5778eefc5cc8980a32012f7ff3
Dan Riley <dklunariley@gmail.com>
2017-02-02 12:32:28 -0600

    Notes for objects and methods.

--- public/js/clock.js
+++ public/js/clock.js
@@ -1,3 +1,14 @@
 /**
  * Created by macbookair on 2/2/17.
  */
+/***************  Face object ************/
+// insert an incremented number every 15 degrees. (Hour makers)
+// insert a tick mark every 3.75 degrees. (Fifteen minute intervals)
+
+/****************  API object ***********/
+// Api request for sunrise and sunset. (On start or midnight update)
+// Insert marks for sunrise and sunset.
+// Equinox and solstice colors (Eq and sol API?)
+
+/****************  Hand object **********/
+// One hand to rule them all.
