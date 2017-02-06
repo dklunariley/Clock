1caa59b62efe002a699180a1604b14435c35103f
Dan Riley <dklunariley@gmail.com>
2017-02-06 15:21:12 -0600

    Working on a better system for number tick placement.

--- public/js/clock.js
+++ public/js/clock.js
@@ -22,26 +22,61 @@
 
 // Everything here replicated in an analog style.
 // And by the way this is a 24 hour clock with midnight (0:00) at the bottom.
+(function  () {
+    var $hour_ind = $("#hour-ind"),
+        secs = 0;
 
-var $hour_ind = $("#hour-ind"),
-    secs = 0;
+    updateSecs();
 
-updateSecs();
+    function updateSecs() {
+        var dt = new Date();
+        secs = dt.getSeconds() + (60 * (dt.getMinutes() + (60 * dt.getHours())));
+    }
 
-function updateSecs() {
-    var dt = new Date();
-    secs = dt.getSeconds() + (60 * (dt.getMinutes() + (60 * dt.getHours())));
-    // console.log("From Date (): " + secs);
-}
+    function setIndicator () {
+        secs += 1;
+        var indicator = secs * 0.00416666666; // 0.00416666666 (yes, theoretical) is the second arc when 360 = 1 day.
+        $hour_ind.css('transform','rotate(' + indicator + 'deg)');
+        if (secs == 86399){updateSecs()}
+    }
 
-function setIndicator () {
-    secs += 1;
-    var indicator = secs * 0.00416666666; // 0.00416666666 is the second arc when 360 = 1 day.
-    $hour_ind.css('transform','rotate(' + indicator + 'deg)');
-    if (secs == 86399){updateSecs()}
-    // console.log("Calculated: " + secs);
-}
+    setInterval(setIndicator, 1000);
 
-setInterval(setIndicator, 1000);
+    var radius = 350;
+    var outerRadius = radius - 10;
+        var numbers = document.getElementById('numbers');
+    var ticks = document.getElementById('ticks');
+    var mark;
+    var rotation;
+    var number;
+    var angle;
+        function cE(type) {
+        return document.createElementNS("http://www.w3.org/2000/svg", type);
+    }
 
+    function createMark(group, outerRadius, length, rotation) {
+        var mark = cE('line');
+        mark.setAttribute('x1', outerRadius - length);
+        mark.setAttribute('x2', outerRadius);
+        mark.setAttribute('y1', '0');
+        mark.setAttribute('y2', '0');
+        mark.setAttribute('transform', 'rotate(' + rotation + ')');
+        group.appendChild(mark);
+    }
 
+    for (var i = 0; i < 24; i++) {
+        number = cE('text');
+        angle = Math.PI / 12 * i;
+        number.setAttribute('x', radius * Math.cos(angle));
+        number.setAttribute('y', radius * Math.sin(angle));
+        number.innerHTML = ((i + 2) % 24);
+        numbers.appendChild(number);
+        rotation = i * 15;
+        createMark(ticks, outerRadius, 16, rotation);
+
+        for (j = 1; j < 12; j++) {
+            createMark(ticks, outerRadius, 8, rotation + j * 6);
+        }
+    }
+
+}());
