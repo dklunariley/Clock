a4ca65481d082ebaebc4a0c9e0bcd22082adb105
Dan Riley <dklunariley@gmail.com>
2017-02-26 13:38:01 -0600

    Added a sun and moon calculating library. API is less than desirable.

--- public/js/clock.js
+++ public/js/clock.js
@@ -11,76 +11,80 @@
 // Season indicator?
 // Temp and weather icons.
 
-(function  () {
-    var staticData = {
-        "version": 2,
-        "locations": [{
-            "id": "750",
-            "geo": {
-                "name": "Lord Howe Island",
-                "state": "Lord Howe Island",
-                "country": {
-                    "id": "au",
-                    "name": "Australia"
-                },
-                "latitude": -31.557,
-                "longitude": 159.086
-            },
-            "astronomy": {
-                "objects": [{
-                    "name": "sun",
-                    "current": {
-                        "azimuth": 89.0,
-                        "altitude": 28.0,
-                        "distance": 147696384
-                    },
-                    "days": [{
-                        "date": "2017-02-12",
-                        "events": [{
-                            "type": "antimeridian",
-                            "hour": 0,
-                            "min": 38
-                        },
-                            {
-                                "type": "twi18_start",
-                                "hour": 4,
-                                "min": 31
-                            },
-                            {
-                                "type": "twi12_start",
-                                "hour": 5,
-                                "min": 3
-                            },
-                            {
-                                "type": "twi6_start",
-                                "hour": 5,
-                                "min": 34
-                            },{
-                                "type": "rise",
-                                "hour": 7,
-                                "min": 14,
-                                "azimuth": 106.8
-                            },{
-                                "type": "meridian",
-                                "hour": 12,
-                                "min": 38,
-                                "altitude": 72.1,
-                                "distance": 147673000
-                            },{
-                                "type": "set",
-                                "hour": 18,
-                                "min": 23,
-                                "azimuth": 253.5
-                            }],
-                        "daylength": "13:17",
-                        "moonphase": "waninggibbous"
-                    },{
 
-                    }]
-                }]
-            }
-        }]
-    };
+
+(function  () {
+
+    //
+    // var staticData = {
+    //     "version": 2,
+    //     "locations": [{
+    //         "id": "750",
+    //         "geo": {
+    //             "name": "Lord Howe Island",
+    //             "state": "Lord Howe Island",
+    //             "country": {
+    //                 "id": "au",
+    //                 "name": "Australia"
+    //             },
+    //             "latitude": -31.557,
+    //             "longitude": 159.086
+    //         },
+    //         "astronomy": {
+    //             "objects": [{
+    //                 "name": "sun",
+    //                 "current": {
+    //                     "azimuth": 89.0,
+    //                     "altitude": 28.0,
+    //                     "distance": 147696384
+    //                 },
+    //                 "days": [{
+    //                     "date": "2017-02-12",
+    //                     "events": [{
+    //                         "type": "antimeridian",
+    //                         "hour": 0,
+    //                         "min": 38
+    //                     },
+    //                         {
+    //                             "type": "twi18_start",
+    //                             "hour": 4,
+    //                             "min": 31
+    //                         },
+    //                         {
+    //                             "type": "twi12_start",
+    //                             "hour": 5,
+    //                             "min": 3
+    //                         },
+    //                         {
+    //                             "type": "twi6_start",
+    //                             "hour": 5,
+    //                             "min": 34
+    //                         },{
+    //                             "type": "rise",
+    //                             "hour": 7,
+    //                             "min": 14,
+    //                             "azimuth": 106.8
+    //                         },{
+    //                             "type": "meridian",
+    //                             "hour": 12,
+    //                             "min": 38,
+    //                             "altitude": 72.1,
+    //                             "distance": 147673000
+    //                         },{
+    //                             "type": "set",
+    //                             "hour": 18,
+    //                             "min": 23,
+    //                             "azimuth": 253.5
+    //                         }],
+    //                     "daylength": "13:17",
+    //                     "moonphase": "waninggibbous"
+    //                 },{
+    //
+    //                 }]
+    //             }]
+    //         }
+    //     }]
+    // };
 
     var $hour_ind = $("#hour-ind"),
         $sunRiseIndicator = $('#sunRiseIndicator'),
@@ -102,15 +106,31 @@
     testFunction();
     function testFunction()
     {
+        var date = Date.now();
+        var lat =  29.4980603;
+        var lng =  -98.6093386;
+        var times = SunCalc.getTimes(date, lat, lng);
+
+        var sunriseDateAndTime = times.sunrise;
+        var stringDateAndTime =  sunriseDateAndTime.toString();
+        var arrayifyHoursAndMin = stringDateAndTime.split(" ");
+        var hourAndMin = arrayifyHoursAndMin[4];
+        var separatedHoursAndMin = hourAndMin.split(":");
+        var sepHours = separatedHoursAndMin[0];
+        var sepMin = separatedHoursAndMin[1];
+        console.log(sepHours + " " + sepMin);
+        var moonTimes = SunCalc.getMoonTimes(date, lat, lng);
+        console.log("Moon times: " + moonTimes);
         // $info.done(function() {
         // console.log("Received request");
         // info = $info.responseText;
         // var json = JSON.parse($info);
-        var days = staticData.locations[0].astronomy.objects[0].days[0];
-        var sunRise = (60 * (days.events[4].min)) + (60 * (60 * (days.events[4].hour)));
-        var sunSet = (60 * (days.events[6].min)) + (60 * (60 * (days.events[6].hour)));
+
+        // var days = staticData.locations[0].astronomy.objects[0].days[0];
+        var sunRise = (60 * (sepMin) + (60 * (60 * sepHours)));
+        // var sunSet = (60 * (days.events[6].min)) + (60 * (60 * (days.events[6].hour)));
         var riseDeg = secondsToDegrees(sunRise);
-        var setDeg = secondsToDegrees(sunSet);
+        // var setDeg = secondsToDegrees(sunSet);
         setIndicator(riseDeg, $sunRiseIndicator);
         setIndicator(setDeg, $sunSetIndicator);
         document.getElementById("area-of-sky").setAttribute("d", describeArc(400, 400, 190, riseDeg, setDeg));
